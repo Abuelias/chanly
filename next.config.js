08025d72eb92a33f6cecc49ba69acb965a742b33
Abuelias <49498078+Abuelias@users.noreply.github.com>
2026-01-09 02:13:07 +0300

    Enable React strict mode
    Enable React strict mode in Next.js configuration.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   i18n: {
     locales: ['en', 'ar'],
     defaultLocale: 'en',
   },
-  output: 'standalone', // مهم لتصدير static
-  trailingSlash: true,  // يسهل التعامل مع export paths
 }
 
 module.exports = nextConfig
