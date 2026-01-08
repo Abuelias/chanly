6925c606308ccb394fd2ed00586ea3636baf0ecc
Abuelias <49498078+Abuelias@users.noreply.github.com>
2026-01-09 02:19:54 +0300

    Update next.config.js configuration settings
    Removed reactStrictMode and added output configuration.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   i18n: {
     locales: ['en', 'ar'],
     defaultLocale: 'en',
   },
+  output: undefined, // أهم خطوة: نتركها SSR/Hybrid
+  // trailingSlash: true, // يمكن حذفه لأنه لا يحتاج للتصدير
 }
 
 module.exports = nextConfig
